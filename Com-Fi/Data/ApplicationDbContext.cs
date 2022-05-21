a116d1928fc6508b8599d0465f9693b5f0a1343a
dgraca <danielgrascas@gmail.com>
2022-05-21 11:20:17 +0100

    Genres table seed

--- Com-Fi/Data/ApplicationDbContext.cs
+++ Com-Fi/Data/ApplicationDbContext.cs
@@ -4,9 +4,27 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Com_Fi.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
-    {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options){ }
+    public class ApplicationDbContext : IdentityDbContext {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options){}
+
+        /// <summary>
+        /// Executes this chunk of code before the creation of the models.
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            // imports the previous execution of this method
+            base.OnModelCreating(modelBuilder);
+
+            // create the seed of Genres table
+            modelBuilder.Entity<Genres>().HasData(
+               new Genres { Id = 1, Title = "Jazz" },
+               new Genres { Id = 2, Title = "Blues" },
+               new Genres { Id = 3, Title = "Rock" },
+               new Genres { Id = 4, Title = "Folk" },
+               new Genres { Id = 5, Title = "Classical" }
+            );
+        }
+
         // Define tables on the database
         public DbSet<Artists> Artists { get; set; }
         public DbSet<Albums> Albums { get; set; }
---
+++ Com-Fi/Data/Migrations/20220521101827_AddGenresSeed.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Com_Fi.Data.Migrations
+{
+    public partial class AddGenresSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Genres",
+                columns: new[] { "Id", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Jazz" },
+                    { 2, "Blues" },
+                    { 3, "Rock" },
+                    { 4, "Folk" },
+                    { 5, "Classical" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
